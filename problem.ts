13e57a32bb033178419c4613a248d16e118f70dd
Md Sakib Mia <sakib01181@gmail.com>
2026-05-02 01:24:04 +0600

    title case solved

--- problem.ts
+++ problem.ts
@@ -1,1 +1,14 @@
-console.log("hekkko")
+function titleCase(sentence: string) {
+    const tCase = []
+    const split = sentence.split(" ")
+    const changeCase = split.map(item => {
+        var text = item.split("")
+        const firstText = text[0].toUpperCase()
+        text.shift()
+        const newArray = [firstText, ...text]
+        return newArray.join("")
+    })
+
+    return changeCase.join(" ")
+}
+console.log(titleCase("my name is shakib"))
